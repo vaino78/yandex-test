2b1da2554e35ec144d6c1916061db75f38351c8d
Ivan Danilov <vaino78@yandex.ru>
2014-08-24 14:41:20 +0400

    Переделка структуры

--- js/presentation.js
+++ js/presentation.js
@@ -17,8 +17,24 @@ $(function(){
 
 	var ViewModel = Backbone.Model.extend({
 		"defaults"    : {
-			"index"  : 0,
-			"slides" : new SlideCollection([])
+			"index"      : 0,
+			"slide"      : null,
+			"canBack"    : false,
+			"canForward" : false,
+			"total"      : null,
+			"slides"     : new SlideCollection([])
+		},
+
+		"initialize"  : function() {
+			this.on('change:index', this.checkState);
+		},
+
+		"checkState"  : function() {
+			this.set({
+				"slide"      : this.attributes.slides.at(this.attributes.index),
+				"canBack"    : (this.attributes.index > 0),
+				"canForward" : (this.attributes.index < this.attributes.slides.length - 1)
+			});
 		},
 
 		"total"       : function() {
@@ -30,47 +46,31 @@ $(function(){
 		},
 
 		"back"        : function() {
-			if(!this.canPrevoius())
+			if(!this.attributes.canBack)
 				return false;
 			this.set('index', (this.attributes.index-1));
 			return true;
 		},
 
 		"forward"     : function() {
-			if(!this.canNext())
+			if(!this.attributes.canForward)
 				return false;
 			this.set('index', (this.attributes.index+1));
 			return true;
 		},
 
 		"begin"       : function() {
-			if(!this.canBegin())
+			if(!this.attributes.canBack)
 				return false;
 			this.set('index', 0);
 			return true;
 		},
 
 		"end"         : function() {
-			if(!this.canEnd())
+			if(!this.attributes.canForward)
 				return false;
 			this.set('index', this.total()-1);
 			return true;
-		},
-
-		"canNext"     : function() {
-			return (this.attributes.index < this.total()-1);
-		},
-
-		"canPrevious" : function() {
-			return (this.attributes.index > 0);
-		},
-
-		"canBegin"    : function() {
-			return this.canPrevious();
-		},
-
-		"canEnd"      : function() {
-			return this.canNext();
 		}
 	});
 
@@ -81,18 +81,51 @@ $(function(){
 		"events"     : {
 			"click .presentation-nav-back"  : "back",
 			"click .presentation-nav-forw"  : "forward",
-			"click .presentation-nav-begin" : 
+			"click .presentation-nav-begin" : "begin",
+			"click .presentation-nav-end"   : "end"
 		},
 
 		"initialize" : function() {
 			this.template = $.trim($('#presentationInnerTemplate').html());
-			console.log(this);
+
+			this.model.on('change:canBack',    this._canBack);
+			this.model.on('change:canForward', this._canForward);
+			this.model.on('change:slide',      this._slide);
+
+			this.model.checkState();
 
 			this.render();
 		},
 
 		"render"     : function() {
 			this.$el.html(_.template(this.template, {}));
+		},
+
+		"back"       : function() {
+			this.model.back();
+		},
+
+		"forward"    : function() {
+			this.model.forward();
+		},
+
+		"begin"      : function() {
+			this.model.begin();
+		},
+
+		"end"        : function() {
+			this.model.end();
+		},
+
+		"_canBack"   : function(value) {
+			this.$('.presentation-nav-back, .presentation-nav-begin').attr('disabled', !value);
+		},
+
+		"_canForward": function(value) {
+			this.$('.presentation-nav-forw, .presentation-nav-end').attr('disabled', !value);
+		},
+
+		"_slide"     : function(value) {
 		}
 	});
 
