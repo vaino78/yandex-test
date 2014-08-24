fe6c0a2037b49d8464c314b028df1abd3b346c41
Ivan Danilov <vaino78@yandex.ru>
2014-08-24 11:45:21 +0400

    Переделка модели

--- js/presentation.js
+++ js/presentation.js
@@ -11,14 +11,62 @@ $(function(){
 		}
 	});
 
-	var Presentation = Backbone.Collection.extend({
+	var SlideCollection = Backbone.Collection.extend({
 		"model" : Slide
 	});
 
 	var ViewModel = Backbone.Model.extend({
-		"defaults" : {
-			"presentation" : null,
-			"slide"        : null
+		"defaults"    : {
+			"index"  : 0,
+			"slides" : new SlideCollection([]);
+		},
+
+		"total"       : function() {
+			return this.attributes.slides.length;
+		},
+
+		"back"        : function() {
+			if(!this.canPrevoius())
+				return false;
+			this.set('index', (this.attributes.index-1));
+			return true;
+		},
+
+		"forward"     : function() {
+			if(!this.canNext())
+				return false;
+			this.set('index', (this.attributes.index+1));
+			return true;
+		},
+
+		"begin"       : function() {
+			if(!this.canBegin())
+				return false;
+			this.set('index', 0);
+			return true;
+		},
+
+		"end"         : function() {
+			if(!this.canEnd())
+				return false;
+			this.set('index', this.total()-1);
+			return true;
+		},
+
+		"canNext"     : function() {
+			return (this.attributes.index < this.total()-1);
+		},
+
+		"canPrevious" : function() {
+			return (this.attributes.index > 0);
+		},
+
+		"canBegin"    : function() {
+			return this.canPrevious();
+		},
+
+		"canEnd"      : function() {
+			return this.canNext();
 		}
 	});
 
